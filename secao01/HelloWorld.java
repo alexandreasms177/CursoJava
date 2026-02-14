4cdc9087a17c1d1727dbb7d2d0a975b7a2e2d8c2
alexandre <alexandreasms177@gmail.com>
2026-02-14 15:26:30 -0300

    aprencendo o conceito de variavel

--- secao01/HelloWorld.java
+++ secao01/HelloWorld.java
@@ -3,7 +3,7 @@ package secao01;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("olá mundo");
+        System.out.println("olá mundo!");
 
     } 
     
---
+++ secao02/variavel.java
@@ -0,0 +1,13 @@
+package secao02;
+
+public class variavel {
+
+    public static void main(String[] args) {
+        
+        String nome = "Alexandre";
+
+        System.out.println(nome);
+
+    }
+
+}
