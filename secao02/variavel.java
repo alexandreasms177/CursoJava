444c698ff54471e45a9651f5022817d2493b5e19
alexandre <alexandreasms177@gmail.com>
2026-02-14 16:19:24 -0300

    meu primeiro olá mundo em java

--- secao02/variavel.java
+++ secao02/variavel.java
@@ -1,12 +1,18 @@
 package secao02;
 
+import java.time.format.SignStyle;
+
 public class variavel {
 
     public static void main(String[] args) {
         
         String nome = "Alexandre";
 
-        System.out.println(nome);
+        System.out.println("nome:"+ nome);
+
+        int idade = 19;
+
+        System.out.println("idade:"+ idade);
 
     }
 
