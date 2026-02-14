650cf32fddf997ed73846fdfdca82b1b090a6b73
alexandre <alexandreasms177@gmail.com>
2026-02-14 16:32:26 -0300

    meu primeiro olá mundo

--- secao01/HelloWorld.java
+++ secao01/HelloWorld.java
@@ -3,8 +3,8 @@ package secao01;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("olá mundo!");
+        System.out.println("olá mundo");
 
     } 
-    
+
 }
