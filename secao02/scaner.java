7ba5ea23d41a6a7504d49c169e4c3a04f7e372e7
alexandre <alexandreasms177@gmail.com>
2026-02-15 02:00:27 -0300

    variaveis e scanner

--- secao02/scaner.java
+++ secao02/scaner.java
@@ -16,10 +16,22 @@ public class scaner {
         //exibe o valor
         System.out.println("ola "+ nome);
 
-        //2- next int
+        //nextInt
         System.out.println("Digite um numero: ");
         int n1 = scanner.nextInt();
         System.out.println("Seu numero é: "+ n1);
         
+        //2- priblema no next line
+
+        System.out.println("Digite um numero: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("digite um texto:");
+        String txt = scanner.nextLine();
+
+        System.out.println("os dados sao: n = "+n+" e txt = "+txt);
+
+        //fechamento do scanner para evitar memory leak
+        scanner.close();
     }
 }
